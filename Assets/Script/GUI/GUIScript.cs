61a110c21721865aba1906a6361f4885255cf2ef
Christian Gomez <gomez-christian@gmo.jp>
2016-12-14 16:30:36 +0900

    webSocket message comunication done

---
+++ Assets/Script/GUI/GUIScript.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GUIScript : MonoBehaviour {
+
+    public static GUIScript Instance;
+
+    public string username;
+    public string password;
+    public string room;
+    public string file;
+    [SerializeField]public string token;
+    public GameObject canvas;
+    public GameObject panel;
+
+    void Awake ()
+    {
+        if (Instance == null)
+        {
+            panel = canvas.transform.Find("SignIn").gameObject;
+            panel.SetActive(true);
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        } else if (Instance != this) {
+            Destroy(gameObject);
+        }
+    }
+
+	public void Join ()
+    {
+        room = GameObject.Find("RoomField").GetComponent<InputField>().text.ToString();
+        GameObject.Find("UserController").GetComponent<Socket>().enabled = true;
+        SceneManager.LoadScene("Main");
+    }
+
+    public void SignIn()
+    {
+        username = GameObject.Find("UserField").GetComponent<InputField>().text.ToString();
+        password = GameObject.Find("PwdField").GetComponent<InputField>().text.ToString();
+        WWWForm form = new WWWForm();
+        form.AddField("user_name",username);
+        form.AddField("password",password);
+        WWW www = new WWW("http://192.168.200.203:4000/api/login", form);
+        StartCoroutine(LogIn(www));
+    }
+
+    public void SignUp()
+    {
+        panel.SetActive(false);
+        panel = canvas.transform.Find("SignUp").gameObject;
+        panel.SetActive(true);
+    }
+
+    public void Back()
+    {
+        panel.SetActive(false);
+        panel = canvas.transform.Find("SignIn").gameObject;
+        panel.SetActive(true);
+    }
+
+    public void SignOut()
+    {
+        username = "";
+        password = "";
+        room = "";
+        file = "";
+        token = "";
+        Back();
+    }
+
+    public void OkButton()
+    {
+        canvas.transform.Find("Error").gameObject.SetActive(false);
+    }
+	
+    IEnumerator LogIn(WWW www)
+    {
+        yield return www;
+
+        if (!string.IsNullOrEmpty(www.error))
+        {
+            Debug.Log(www.error);
+            yield break;
+        }
+        JSONObject json = new JSONObject(www.text);
+
+        token = json.getString("token");
+
+        if (token != "")
+        {
+            panel.SetActive(false);
+            panel = canvas.transform.Find("Menu").gameObject;
+            panel.SetActive(true);
+        }else
+        {
+            canvas.transform.Find("Error").gameObject.SetActive(true);
+        }
+
+        Debug.Log(json);
+    }
+
+    public void selection(Dropdown dropdown)
+    {
+        switch (dropdown.value)
+        {
+            case 0:
+                panel.transform.Find("File").gameObject.SetActive(true);
+                break;
+            case 1:
+                panel.transform.Find("File").gameObject.SetActive(false);
+                break;
+        }
+    }
+}
---
+++ Assets/Script/Socket.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using WebSocketSharp;
+using WebSocketSharp.Net;
+
+public class Socket : MonoBehaviour
+{
+
+    WebSocket ws;
+    GameObject userController;
+    public GUIScript guiContoller;
+
+    void Start()
+    {
+        string _token = guiContoller.token;
+        ws = new WebSocket("ws://192.168.200.203:4000/socket/websocket?token=" + _token);
+
+        ws.OnOpen += (sender, e) =>
+        {
+            Debug.Log("WebSocket Open");
+            ws.Send("{\"topic\":\"rooms: vr_presentation\",\"ref\":1, \"payload\":{},\"event\":\"phx_join\"}");
+        };
+
+        ws.OnMessage += (sender, e) =>
+        {
+            Debug.Log("WebSocket Message Data: " + e.Data);
+        };
+
+        ws.OnError += (sender, e) =>
+        {
+            Debug.Log("WebSocket Error Message: " + e.Message);
+        };
+
+        ws.OnClose += (sender, e) =>
+        {
+            Debug.Log("WebSocket Close");
+        };
+
+        ws.Connect();
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyUp("d"))
+        {
+            string stream = "{\"topic\":\"rooms: vr_presentation\", \"ref\":1, \"payload\":{\"user\":\"a @a\",\"body\":\"chat - message\"}, \"event\":\"new:message\"}";
+            
+            ws.Send(stream);
+        }
+
+    }
+
+    void OnDestroy()
+    {
+        ws.Close();
+        ws = null;
+    }
+
+    enum Type
+    {
+        REGISTER = 1,
+        SEND_DATA = 2,
+
+
+    }
+
+}
