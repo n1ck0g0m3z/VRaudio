e7b1c243a112894eb60ca79487576cded07fe5fa
Christian Gomez <gomez-christian@gmo.jp>
2016-12-21 10:16:34 +0900

    add middleware to log page

---
+++ Assets/Script/GUIMiddleware.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GUIMiddleware : MonoBehaviour {
+    protected GUIScript gui;
+	// Use this for initialization
+	void Start () {
+        gui = (GUIScript)FindObjectOfType(typeof(GUIScript));
+	}
+
+    public void Join()
+    {
+        gui.Join();
+    }
+
+    public void SignIn()
+    {
+        gui.SignIn();
+    }
+
+    public void SignUp()
+    {
+        gui.SignUp();
+    }
+
+    public void RegisterUser()
+    {
+        gui.RegisterUser();
+    }
+
+    public void Back()
+    {
+        gui.Back();
+    }
+
+    public void SignOut()
+    {
+        gui.SignOut();
+    }
+
+    public void OkButton()
+    {
+        gui.OkButton();
+    }
+
+    public void selection(Dropdown dropdown)
+    {
+        gui.selection(dropdown);
+    }
+
+    public void Search()
+    {
+        gui.Search();
+    }
+}
